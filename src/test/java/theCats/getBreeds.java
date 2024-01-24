c116e497017e65e8df38065cafb1ddbca0ef77d6
apoorvSinha <sinhapoo@amazon.com>
2024-01-24 23:30:30 +0530

    added file writing & exceptions

--- src/test/java/theCats/getBreeds.java
+++ src/test/java/theCats/getBreeds.java
@@ -1,16 +1,24 @@
 package theCats;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.FileHandler;
 
 import static io.restassured.RestAssured.given;
 
 public class getBreeds {
-    public static void main(String[] args) {
+    static JsonPath js;
+    public static void main(String[] args) throws IOException {
         RestAssured.baseURI = "https://api.thecatapi.com/";
-        getImages();
+        StringBuffer str = new StringBuffer();
+        for(String string: getImages()){
+            str.append(string +  "\n");
+        }
+        new getBreeds().writeFiles(String.valueOf(str));
     }
 
 
@@ -28,25 +36,27 @@ public class getBreeds {
         }
     }
 
-    private void writeFiles(String str){
+    private void writeFiles(String str) throws IOException {
         BufferedWriter writer;
         try {
             writer = new BufferedWriter(new FileWriter("./src/test/java/theCats/abc.txt"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        try {
-            writer.write(str);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        writer.write(str);
+        writer.close();
     }
 
-    public static void getImages(){
-        given().log().all().header("x-api-key", new getBreeds().getKey())
+    public static String[] getImages(){
+        String response = given().log().all().header("x-api-key", new getBreeds().getKey())
                 .header("Content-Type", "application/json")
-                .queryParam("limit", 3)
+                .queryParam("limit", 10)
                 .when().get("v1/images/search")
-                .then().log().all().assertThat().statusCode(200);
+                .then().log().all().assertThat().statusCode(200).extract().asString();
+        js = new JsonPath(response);
+        String catUrls = js.getString("url");
+        catUrls = catUrls.split("\\[")[1];
+        catUrls = catUrls.split("]")[0];
+        return catUrls.split(",");
     }
 }
